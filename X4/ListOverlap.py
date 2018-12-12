8b1189134592bca61001840c79188f40d6e30b26
benjaminreis <benjaminreis@gmail.com>
2018-12-12 03:05:44 -0600

    starting on list overlap

---
+++ X4/ListOverlap.py
@@ -0,0 +1,15 @@
+class ListOverlap:
+
+    def __init__(self):
+        self._list_one = []
+        self._list_two = []
+
+    def GetListsFromUser(self):
+        return True
+
+    def FindIntersection(self):
+        return 1
+
+    def PrintResults(self):
+        #TODO BEN look into doing interesection in python
+        return ""  #http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
