1d53c04f2ca7b87aa49491fda4553b2edae9bdd0
benjaminreis <benjaminreis@gmail.com>
2018-12-12 03:27:11 -0600

    listoverlap trivial solution

--- X4/ListOverlap.py
+++ X4/ListOverlap.py
@@ -1,15 +1,28 @@
+
 class ListOverlap:
 
     def __init__(self):
         self._list_one = []
         self._list_two = []
 
-    def GetListsFromUser(self):
-        return True
+    def GetListsLengthsFromUser(self):
+        print("Welcome to \"List Overlap!\"")
+        print("The length of the lists (2-100) determines the range of random numbers for each value in the list/array.")
+        length = input("How long do you want the lists/arrays to be? (2-100")
+        GenerateLists(length)
+        FindIntersection()
 
-    def FindIntersection(self):
-        return 1
 
-    def PrintResults(self):
-        #TODO BEN look into doing interesection in python
-        return ""  #http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+def FindIntersection(self):
+    intersection = set(self._list_one).intersection(self._list_two)
+    print(intersection)
+
+    # def PrintResults(self):
+    #     #TODO BEN look into doing interesection in python
+    #     return ""  #http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+
+
+def GenerateLists(self, length):
+    for x in range(length):
+        self._list_one.append().randint(0, length)
+        self._list_two.append().randint(0, length)
