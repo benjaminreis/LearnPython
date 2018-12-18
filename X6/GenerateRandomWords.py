4412ada36cc78a5dda3ac49e8fcee8b3088378a3
benjaminreis <benjaminreis@gmail.com>
2018-12-18 17:20:56 -0600

    eh, trying to get back into it after a busy day :-)

---
+++ X6/GenerateRandomWords.py
@@ -0,0 +1,4 @@
+class GenerateRandomWords:
+    def __init__(self, word_length, word_count):
+        self.__word_length = word_length    #this length is for the length of characters in each word
+        self.__word_count = word_count
