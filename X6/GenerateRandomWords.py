63d50c7a1019ff92190c07d6c2cf6c464bd1d339
benjaminreis <benjaminreis@gmail.com>
2018-12-30 23:23:47 -0600

    writing out method stubs

--- X6/GenerateRandomWords.py
+++ X6/GenerateRandomWords.py
@@ -2,3 +2,14 @@ class GenerateRandomWords:
     def __init__(self, word_length, word_count):
         self.__word_length = word_length    #this length is for the length of characters in each word
         self.__word_count = word_count
+
+    def GenerateRandomWords(self):
+        words = ""
+        for i in range(self.__word_count):
+                #call GenerateWord for each word, appeld it to the string
+        return words
+#TODO BEN finish this method
+    
+    def GenerateWord(self):
+        for i in range(self.__word_length):
+    # TODO BEN finish this method
